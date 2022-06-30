3f2dac7a53a689628a53a33cc9271cb90a18a82b
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-06-30 21:34:26 +0200

    wip

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Entity\User;
 use App\Form\SearchProjectType;
 use App\Repository\ProjectRepository;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function view(ProjectRepository $projectRepository, Request $request): Response
+    public function view(ProjectRepository $projectRepository, TechnologyRepository $technologyRepository, Request $request): Response
     {
         $searchForm = $this->createForm(SearchProjectType::class);
         $searchForm->handleRequest($request);
@@ -28,17 +29,20 @@ class DashboardController extends AbstractController
             }
             return $this->render('dashboard.html.twig', [
                 'project' => $program,
-                'form' => $searchForm->createView()]);
+                'form' => $searchForm->createView(),
+                'skills' => $technologyRepository->findAll(),
+                ]);
         }
 
         return $this->render('dashboard.html.twig', [
             'project' => $projectRepository->findAll(),
-            'form' => $searchForm->createView()
+            'form' => $searchForm->createView(),
+            'skills' => $technologyRepository->findAll(),
         ]);
     }
 
     #[Route('/favourites', name: 'app_favourites')]
-    public function favourites(ProjectRepository $projectRepository, Request $request): Response
+    public function favourites(ProjectRepository $projectRepository, TechnologyRepository $technologyRepository, Request $request): Response
     {
         $searchForm = $this->createForm(SearchProjectType::class);
         $searchForm->handleRequest($request);
@@ -52,12 +56,74 @@ class DashboardController extends AbstractController
             }
             return $this->render('dashboard.html.twig', [
                 'project' => $program,
-                'form' => $searchForm->createView()]);
+                'form' => $searchForm->createView(),
+                'skills' => $technologyRepository->findAll(),
+                ]);
         }
 
         return $this->render('dashboard.html.twig', [
             'project' => $this->getUser()->getFavouriteProjects(),
-            'form' => $searchForm->createView()
+            'form' => $searchForm->createView(),
+            'skills' => $technologyRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/mostliked', name: 'app_mostliked')]
+    public function mostliked(ProjectRepository $projectRepository, TechnologyRepository $technologyRepository, Request $request): Response
+    {
+        $searchForm = $this->createForm(SearchProjectType::class);
+        $searchForm->handleRequest($request);
+
+        if( $searchForm->isSubmitted() && $searchForm->isValid() ) {
+            $search = $searchForm->get('title')->getData();
+            $program = $projectRepository->findTitleTag($search);
+            if(empty($program)) {
+                $this->addFlash('primary', 'No project fund');
+                return $this->redirectToRoute('dashboard');
+            }
+            return $this->render('dashboard.html.twig', [
+                'project' => $program,
+                'form' => $searchForm->createView(),
+                'skills' => $technologyRepository->findAll(),
+            ]);
+
+        }
+        $projects = $projectRepository->findAll();
+        usort($projects, function (Project $p1, Project $p2){
+            return count($p2->getUsersWhoLiked()) <=> count($p1->getUsersWhoLiked());
+        });
+
+        return $this->render('dashboard.html.twig', [
+            'project' => $projects,
+            'form' => $searchForm->createView(),
+            'skills' => $technologyRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/projects/{techno}', name: 'app_projects_techno')]
+    public function showByTechno(int $techno, TechnologyRepository $technologyRepository, ProjectRepository $projectRepository, Request $request): Response
+    {
+        $searchForm = $this->createForm(SearchProjectType::class);
+        $searchForm->handleRequest($request);
+
+        if( $searchForm->isSubmitted() && $searchForm->isValid() ) {
+            $search = $searchForm->get('title')->getData();
+            $program = $projectRepository->findTitleTag($search);
+            if(empty($program)) {
+                $this->addFlash('primary', 'No project fund');
+                return $this->redirectToRoute('dashboard');
+            }
+            return $this->render('dashboard.html.twig', [
+                'project' => $program,
+                'form' => $searchForm->createView(),
+                'skills' => $technologyRepository->findAll(),
+                ]);
+        }
+
+        return $this->render('dashboard.html.twig', [
+            'project' => $technologyRepository->findOneBy(['id' => $techno])->getProjects(),
+            'form' => $searchForm->createView(),
+            'skills' => $technologyRepository->findAll(),
         ]);
     }
 }
